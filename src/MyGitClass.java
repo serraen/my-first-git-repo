94e01a316fb79564480794e6e936e44133dace87
serraen <lavanta1134@gmail.com>
2022-07-16 21:47:04 +0200

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,5 +1,6 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub");
+        System.out.println(" I am adding one more");
     }
 }
