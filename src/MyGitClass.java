6916701da498e02c1fcd3a5adc50612ea4efe8a5
serraen <lavanta1134@gmail.com>
2022-07-17 14:32:11 +0200

    second commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,6 +1,7 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub");
-        System.out.println(" I am adding one more");
+        System.out.println("I am adding one more");
+        System.out.println("deneme");
     }
 }
