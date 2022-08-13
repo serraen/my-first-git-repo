c7209790feecd477ffc36b6e17bb011cb437f2d8
serraen <lavanta1134@gmail.com>
2022-08-13 21:00:00 +0200

    deneme

--- src/Serra.java
+++ src/Serra.java
@@ -2,5 +2,9 @@ public class Serra {
     // new
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hi!");
     }
 }
+// ==> git add .
+//  	     ==> git commit -m "message"
+//  	     ==> git push
